8d0ee7b6367a766958e53f3107cd8cb293221618
daviaraujo99 <62964272+daviaraujo99@users.noreply.github.com>
2020-06-24 17:57:28 -0300

    Update binary_tree.js

--- js/binary_tree.js
+++ js/binary_tree.js
@@ -99,10 +99,14 @@ class BinaryTree {
     }
 
     postOrderVisitor(node, callback) {
+        //compra se o nó é nulo
         if (node == null)
             return
+        //mostra o valor da direita
         this.postOrderVisitor(node.left, callback)
+       //mostras o valor da esquerda
         this.postOrderVisitor(node.right, callback)
+        //executa a função callback e mostra o conteúdo do nó
         callback(node.content)
     }
 
@@ -114,15 +118,19 @@ class BinaryTree {
     //       3.1 busca de direita
     //       3.2 busca na esquerda
 
+   //busca se o valor que foi inserido está na árvore
     search(value) {
         return this.searchVisitor(this.root, value)
     }
 
     searchVisitor(node, element) {
+       //se é nulo o elemento não existe e retorna falso
         if (node == null)
             return false
+        // se é igual ao conteúdo achou o elemento 
         if (node.content == element)
             return true;
+        //se for maior que o conteúdo, procure na direita, caso não ache,procure na esquerda
         if (element > node.content)
             return this.searchVisitor(node.right, element)
         else
@@ -134,29 +142,44 @@ class BinaryTree {
         this.root = this.removeVisitor(this.root, value)
     }
 
+    // remove um elemento da árvore
     removeVisitor(node, value) {
+        //verifica se o conteúdo é igual ao valor que foi foi escolhido para ser removido
         if (node.content == value) {
+            //verifica se o valor do nó da esquerda é igual ao valor do nó da direita
             if (node.left == node.right) {
                 //nao tem filhos - Grau 0
+                //retorna nulo
                 return null
+            //verifica se o valor do nó da direita é nulo
             } else if (node.right == null) {
                 //não tem filhos na direita, e tem nó na esqueda - Grau 1
+                //retorna o nó da esquerda
                 return node.left
+            //verifica se o valor do nó da direita é nulo
             } else if (node.left == null) {
                 //não tem filhos da esquerda, e tem nó da direita - Grau 1
+               //retorna o nó da direita
                 return node.right
             } else {
                 // tem os dois ramos - Grau 2
+                //newRoot recebe o nó da direita
                 const newRoot = node.right
+                //current recebe o nó da direita
                 let current = node.right;
+                //enquanto current.left for diferente de nulo current recebe o nó da direita
                 while (current.left != null)
                     current = current.left
                 current.left = node.left
+                //retorna newRoot
                 return newRoot;
             }
+        // verifica se o valor é menor do que conteúdo do nó
         } else if (value < node.content) {
+            //remove o valor do nó da da esquerda
             node.left = this.removeVisitor(node.left, value)
         } else {
+            //remove o valor do nó da direita
             node.right = this.removeVisitor(node.right, value)
         }
         return node;
